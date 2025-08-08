91484ea6a569a16b29b1a4db775244b984f4dad2
trkngit <trknsrv@gmail.com>
2025-08-08 13:37:09 +0300

    Initial Commit

---
+++ MarscopeTests/MarscopeTests.swift
@@ -0,0 +1,17 @@
+//
+//  MarscopeTests.swift
+//  MarscopeTests
+//
+//  Created by Tarkan Surav on 8.08.2025.
+//
+
+import Testing
+@testable import Marscope
+
+struct MarscopeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
